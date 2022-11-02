b79a0fd8b25072987fa019c510e6f7f587c29fbd
nivo0616 <nikita.vorobev@netcracker.com>
2022-11-03 00:00:13 +0300

    init project

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './app.module.css';
+
+function App() {
+    return <>
+        <div className={styles.app}>Hello</div>
+    </>;
+}
+
+export default App;
