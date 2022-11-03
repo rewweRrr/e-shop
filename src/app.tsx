e65fd00976eee9bd3641854d49ee24303360d2d7
nivo0616 <nikita.vorobev@netcracker.com>
2022-11-03 15:35:21 +0300

    add prettier

--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styles from './app.module.css';
 
 function App() {
-    return <>
-        <div className={styles.app}>Hello</div>
-    </>;
+    return (
+        <>
+            <div className={styles.app}>Hello</div>
+        </>
+    );
 }
 
 export default App;
